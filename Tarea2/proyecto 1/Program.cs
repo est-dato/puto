f09e4351efcd482ad961c54d7c77fc3e3e0c561b
ignacio Vela <89303905+est-dato@users.noreply.github.com>
2021-10-29 22:08:42 -0300

    Add files via upload

---
+++ Tarea2/proyecto 1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proyecto_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, suma;
+            string linea;
+            Console.Write("Ingrese el primer Valor");
+            linea = Console.ReadLine();
+            num1 = int.Parse(linea);
+            Console.Write("Ingrese un segundo valor: ");
+            linea = Console.ReadLine();
+            num2 = int.Parse(linea);
+            suma = num1 + num2;
+            Console.Write("La suma de los dos valores es: ");
+            Console.WriteLine(suma);
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
