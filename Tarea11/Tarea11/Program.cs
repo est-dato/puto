d92ef51f31d4b81ed2bf6cd247ac411d0a0a0d17
ignacio Vela <89303905+est-dato@users.noreply.github.com>
2021-10-29 22:13:52 -0300

    Add files via upload

---
+++ Tarea11/Tarea11/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+            string linea;
+            Console.Write("Ingrese un numero: ");
+            linea = Console.ReadLine();
+            num1 = int.Parse(linea);
+            Console.Write("Ingrese un segundo numero: ");
+            linea = Console.ReadLine();
+            num2= int.Parse(linea);
+
+
+            if (num1 > num2) 
+            {
+                Console.Write("El numero mayor es: ");
+                Console.WriteLine(num1);
+            }
+            else
+            {
+                Console.Write("El numero mayor es: ");
+                Console.WriteLine(num2);
+            }
+            Console.ReadKey();
+
+
+
+
+
+        }
+    }
+}
---
+++ Tarea12/Tarea12/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, div ;
+            string linea;
+            Console.Write("Ingrese el primer numero: ");
+            linea = Console.ReadLine();
+            num1 = int.Parse(linea);
+            Console.Write("Ingrese el segundo numero: ");
+            linea = Console.ReadLine();
+            num2 = int.Parse(linea);
+            if (num2 == 0)
+            {
+             Console.Write("Error:NO se puede dividir entre cero.");
+            }
+            else
+            {
+                div = num1 / num2;
+                Console.Write("El resultado es :");
+                Console.WriteLine(div);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
